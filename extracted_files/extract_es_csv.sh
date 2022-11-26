7d535d08250c609d6c8a2bf546f324978b9f3b27
sujatha <sujatce@gmail.com>
2022-11-26 09:16:43 +0000

    script changes

--- extracted_files/extract_es_csv.sh
+++ extracted_files/extract_es_csv.sh
@@ -1,1 +1,1 @@
-./es2csv -q '*' -f traceID spanID process.serviceName operationName startTimeMillis startTime tags references -S startTime -u http://18.212.223.207:9200 -i jaeger-span-2022-11-25 -D span -o /home/ubuntu/sujathesis/extracted_files/traffictrace.csv
+/home/ubuntu/.local/bin/es2csv -q '*' -f traceID spanID process.serviceName operationName startTimeMillis startTime tags references -S startTime -u http://18.212.223.207:9200 -i jaeger-span-2022-11-25 -D span -o /home/ubuntu/sujathesis/extracted_files/traffictrace.csv
