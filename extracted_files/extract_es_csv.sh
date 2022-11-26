d202235ecb1fec4d530f843593caaaaf9ffbb7a2
sujatha <sujatce@gmail.com>
2022-11-26 09:13:24 +0000

    Commit

---
+++ extracted_files/extract_es_csv.sh
@@ -0,0 +1,1 @@
+./es2csv -q '*' -f traceID spanID process.serviceName operationName startTimeMillis startTime tags references -S startTime -u http://18.212.223.207:9200 -i jaeger-span-2022-11-25 -D span -o /home/ubuntu/sujathesis/extracted_files/traffictrace.csv
