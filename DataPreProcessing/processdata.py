255e40f5774b6d7f4cfc7989b67603b4e623149f
sujatha <sujatce@gmail.com>
2022-11-27 12:13:49 +0000

    commit

--- DataPreProcessing/processdata.py
+++ DataPreProcessing/processdata.py
@@ -5,13 +5,13 @@ from step4_GenerateAdjacencyMatrix import step4
 from step5_RecordNodeInstances import step5
 from step6_ReturnRPCTrafficFile import step6
 
-test_number = 15
-n_time_interval = 100
+test_number = 30
+n_time_interval = 200
 #filename = "data/bruteForceAttackData_V"+str(test_number)+".csv"
 filename = "data/input/trafficData_V"+str(test_number)+".csv"
 out_filename = 'data/V'+str(test_number)+'_step1_TS_trafficData.csv'
 out_adj_file = '../suja_stgcn/dataset/ms_traffic_W_'+str(test_number)+'.csv'
-out_temporal_file = '../suja_stgcn/dataset/ms_traffic_V'+str(test_number)+'.csv'
+out_temporal_file = '../suja_stgcn/dataset/ms_traffic_V_'+str(test_number)+'.csv'
 
 print('Goal: using the input file ',filename,' , prepare a graphical representation of RPC Node Pair calls and its traffic distribution into File1: adjacency weight matrix(spatial data) and File2:traffic data (Temporal Data)')
 
@@ -33,4 +33,4 @@ print('Step 5 - This step translates traffic file into graphical representation 
 step5(test_number)
 
 print('Step 6 - This step creates Temporal file for traffic data by translating the traffic timestamps into given intervals and populating the total traffic within each timeslot for each RPC Node Pair-Traffic Node - This is Temporal Traffic file')
-step6(test_number,n_time_interval,out_temporal_file)
+step6(test_number,n_time_interval-1,out_temporal_file)
